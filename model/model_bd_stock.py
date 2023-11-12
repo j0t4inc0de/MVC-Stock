68acda665c8d6b9b899c14ab84a7985a143c55bf
j0t4inc0de <juan.erices04@inacapmail.cl>
2023-11-12 16:06:49 -0300

    conexion a 'base.d'

--- model/model_bd_stock.py
+++ model/model_bd_stock.py
@@ -9,15 +9,15 @@ from PIL import Image, ImageTk
 class ModeloStock:
     def __init__(self):
         # #Tabla Inventario
-        self.conn = sqlite3.connect('data/alanorte.db')
+        self.conn = sqlite3.connect('data/base.db')
         self.cur = self.conn.cursor()
         # self.cur.execute('''CREATE TABLE IF NOT EXISTS producto (id_producto INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, precio REAL)''')
         self.conn.commit()
     
     def insertar_prod(self, nombre, precio, cantidad):
         try:
-            self.cur.execute("INSERT INTO producto (nombre, precio) VALUES (?, ?)", (nombre, precio))
-            self.cur.execute("INSERT INTO existencias (nombre, cantidad) VALUES (?, ?)", (nombre, cantidad))
+            self.cur.execute("INSERT INTO Producto (nombre, precio) VALUES (?, ?)", (nombre, precio))
+            self.cur.execute("INSERT INTO Existencia (nombre, cantidad) VALUES (?, ?)", (nombre, cantidad))
         except sqlite3.Error as e:
             print("Error al insertar datos:", e)
             self.conn.rollback()  # Revertir cualquier cambio en caso de error
