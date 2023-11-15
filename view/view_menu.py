5a730d9d9fb220e66e8d9a293a9b001c66d3b3c7
Juan <juan.erices04@inacapmail.cl>
2023-11-15 18:41:30 -0300

    Vista menu - LISTA

--- view/view_menu.py
+++ view/view_menu.py
@@ -11,8 +11,9 @@ class VistaPrincipal:
         self.modelo_stock = modelo_stock
 
         # Treeview es la tabla donde pondremos los datos de la bd
-        self.treeview = ttk.Treeview(self.ventanaPrincipal, columns=("Nombre", "Precio", "Cantidad", "Estado", "Categoría"), show="headings", height=15)
+        self.treeview = ttk.Treeview(self.ventanaPrincipal, columns=("Nombre", "Precio", "Cantidad", "Estado", "Categoría"), show="headings", height=16)
         self.treeview.heading("Nombre", text="Nombre")
+        self.treeview.column("Nombre", width=280)
         self.treeview.heading("Precio", text="Precio")
         self.treeview.heading("Cantidad", text="Cantidad")
         self.treeview.heading("Estado", text="Estado")
@@ -25,7 +26,7 @@ class VistaPrincipal:
         # ttk.Button(self.ventanaPrincipal, text="Añadir Producto", command=self.abrir_vista_producto).pack(pady=10)
         
         # Entry para buscar - funcionalidad para buscar NO ECHA
-        self.entry_buscar = ttk.Entry(self.ventanaPrincipal, width=60)
+        self.entry_buscar = ttk.Entry(self.ventanaPrincipal, width=46)
         self.entry_buscar.place(x=0, y=18)
 
         # Imagen Lupa
@@ -33,21 +34,21 @@ class VistaPrincipal:
         self.imagen_buscar.thumbnail((30, 30))
         self.imagen_buscar.resize((30, 30))
         self.imagen_buscar = ImageTk.PhotoImage(self.imagen_buscar)
-        self.label_buscar = ttk.Button(self.ventanaPrincipal, image=self.imagen_buscar).place(x=380,y=0)
+        self.label_buscar = ttk.Button(self.ventanaPrincipal, image=self.imagen_buscar).place(x=295,y=0)
 
         # Rodrigo- Se agrega la imagen para boton actualizar
         self.imagen_actualizar = Image.open("view/images/actualizar-512px.png")
         self.imagen_actualizar.thumbnail((30, 30))
         self.imagen_actualizar.resize((30, 30))
         self.imagen_actualizar = ImageTk.PhotoImage(self.imagen_actualizar)
-        self.label_actualizar = ttk.Button(self.ventanaPrincipal, image=self.imagen_actualizar).place(x=440,y=0)
+        self.label_actualizar = ttk.Button(self.ventanaPrincipal, image=self.imagen_actualizar, command=self.mostrar_datos).place(x=360,y=0)
         
         # Rodrigo- Se agrega la imagen para boton Añadir.
         self.imagen_anadir = Image.open("view/images/añadir-512px.png")
         self.imagen_anadir.thumbnail((30, 30))
         self.imagen_anadir.resize((30, 30))
         self.imagen_anadir = ImageTk.PhotoImage(self.imagen_anadir)
-        self.label_anadir = ttk.Button(self.ventanaPrincipal, image=self.imagen_anadir).place(x=515,y=0)
+        self.label_anadir = ttk.Button(self.ventanaPrincipal, image=self.imagen_anadir, command=self.abrir_vista_producto).place(x=450,y=0)
 
         # Rodrigo Se agrega la imagen para boton Eliminar.
         self.imagen_eliminar = Image.open("view/images/eliminar-512px.png")
@@ -55,7 +56,7 @@ class VistaPrincipal:
         self.imagen_eliminar.resize((30, 30))
 
         self.imagen_eliminar = ImageTk.PhotoImage(self.imagen_eliminar)
-        self.label_eliminar = ttk.Button(self.ventanaPrincipal, image=self.imagen_eliminar).place(x=580,y=0)
+        self.label_eliminar = ttk.Button(self.ventanaPrincipal, image=self.imagen_eliminar).place(x=510,y=0)
 
         # Rodrigo Se agrega la imagen para boton Editar.
         self.imagen_editar = Image.open("view/images/editar-512px.png")
@@ -63,7 +64,7 @@ class VistaPrincipal:
         self.imagen_editar.resize((30, 30))
 
         self.imagen_editar = ImageTk.PhotoImage(self.imagen_editar)
-        self.label_editar = ttk.Button(self.ventanaPrincipal, image=self.imagen_editar).place(x=646,y=0)
+        self.label_editar = ttk.Button(self.ventanaPrincipal, image=self.imagen_editar).place(x=570,y=0)
 
     def mostrar_datos(self):
         for item in self.treeview.get_children():
