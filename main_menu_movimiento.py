f317e755cb3ae1f3ead6e5d9d52dba5493782fb3
j0t4inc0de <juan.erices04@inacapmail.cl>
2023-11-30 01:04:03 -0300

    elimine archivos que no usamos

--- main_menu_movimiento.py
+++
@@ -1,9 +0,0 @@
-#Rodrigo - Cristobal Main menu de movimiento
-import tkinter
-from model.model_bd_stock import ModeloStock
-from view.vista_movimiento_menu import VistaMenuMov
-
-if __name__ == "__main__":
-    modelo_stock = ModeloStock('data/base.db')
-    app_principal = VistaMenuMov(modelo_stock)
-    app_principal.ventanaMenuMov.mainloop()
