680e19412ee93308dbe9102b4d38828222b5f309
j0t4inc0de <juan.erices04@inacapmail.cl>
2023-12-16 19:35:21 -0300

    login comentados

--- controller/controlador_login.py
+++ controller/controlador_login.py
@@ -13,29 +13,34 @@ class Controlador:
         self.vista_login.ventana.mainloop()
 
         # self.vista_login.btn_ingresar.configure(command=self.verificar)
-        
+
+    # Verifica el ingreso del usuario y la contraseña.
     def verificar_ingreso(self):
+        # mensaje pa validar
         print("Se apretó el boton verificar en la vista")
+        # Obtiene el usuario y la contraseña del usuario.
         usuario = self.vista_login.entry_usuario.get().lower()
         contraseña = self.vista_login.entry_contraseña.get()
-
+        # Verifica si el usuario y la contraseña son correctos.
         resultado = self.modelo_stock.verificar_credenciales(usuario, contraseña)
-
+        # Verifica si el usuario y la contraseña están vacíos.
         if len(usuario) == 0 or len(contraseña) == 0:
             print('Acceso denegado')
             mb.showerror('Acceso Denegado', 'Usuario/Contraseña no puede estar vacío.')
         else:
             resultado = self.modelo_stock.verificar_credenciales(usuario, contraseña)
+            # Si son correctos, abre la vista principal.
             if resultado:
                 print("Acceso concedido.")
                 self.vista_login.ventana.withdraw()
                 print("Se oculto la ventana login")
                 self.abrir_ventana_principal()
+            # Si son incorrectos, muestra un mensaje de error.
             else:
                 print('Acceso denegado')
                 self.vista_login.entry_contraseña.delete(0, 'end')
                 mb.showerror('Acceso Denegado', 'Usuario/Contraseña incorrectos.')
-
+    # Abre la vista principal de la aplicación.
     def abrir_ventana_principal(self):
         # Cerrar la ventana de inicio de sesión
         self.vista_login.ventana.destroy()
--- view/vista_login.py
+++ view/vista_login.py
@@ -7,22 +7,25 @@ from ttkthemes import ThemedTk
 
 class Login:
     def __init__(self, ventana, modelo_stock):
+        # Inicializa los atributos de la clase.
         self.ventana = ThemedTk(theme="arc")
         self.ventana.title("AlaNorte")
         self.ventana.geometry("400x600")
         self.modelo_stock = modelo_stock
         self.ventana.resizable(width=False, height=False)
-        # Logo
+
+        # Carga el logo de la aplicación.
         self.ruta_imagen_logo = "view/images/logo.png"
         self.logo = Image.open(self.ruta_imagen_logo)
         self.logo = self.logo.resize((250, 250))
         self.logo_imagen = ImageTk.PhotoImage(self.logo)
         self.logo_posicion = Label(self.ventana, image=self.logo_imagen).place(x=72, y=5)
 
+        # Etiquetas de los campos de texto usuario.
         ttk.Label(self.ventana, text="Usuario:").place(x=130, y=270)
         self.entry_usuario = ttk.Entry(self.ventana)
         self.entry_usuario.place(x=130, y=290)
-
+        # Etiquetas de los campos de texto contraseña.
         ttk.Label(self.ventana, text="Contraseña:").place(x=130, y=320)
         self.entry_contraseña = ttk.Entry(self.ventana,show="*")
         self.entry_contraseña.place(x=130, y=340)
